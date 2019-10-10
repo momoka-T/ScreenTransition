14de0ebe21ba97075f9f544997d62a8a0c7605e9
s18b708 <s18b709@shohoku.ac.jp>
2019-10-10 15:21:30 +0900

    kore

--- app/src/main/java/jp/ac/shohoku/O/screentransition/MainView.java
+++ app/src/main/java/jp/ac/shohoku/O/screentransition/MainView.java
@@ -6,11 +6,8 @@ import android.view.View;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
-import android.view.View;
-
 import androidx.annotation.Nullable;
 
 public class MainView extends View{
