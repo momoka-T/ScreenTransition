88f410bd6b40327bb5f9f935fbec61ee6c859bac
s18b708 <s18b709@shohoku.ac.jp>
2019-10-17 16:12:31 +0900

    tuikasitayo!

--- app/src/main/java/jp/ac/shohoku/O/screentransition/MainActivity.java
+++ app/src/main/java/jp/ac/shohoku/O/screentransition/MainActivity.java
@@ -11,31 +11,40 @@ import android.view.View.OnClickListener;
 
 public class MainActivity extends Activity {
     int count = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view);
         //ボタンを押したときにイベントが取得できるようにする
-        Button button1 = (Button)findViewById(R. id. button1);
+        Button button1 = (Button) findViewById(R.id.button1);
         button1.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(count == 1){
+                if (count == 1) {
                     //変更したいレイアウトを取得する
-                    LinearLayout layout = (LinearLayout)findViewById(R.id.linearlayout1);
+                    LinearLayout layout = (LinearLayout) findViewById(R.id.linearlayout1);
                     //レイアウトのビューをすべて削除する
                     layout.removeAllViews();
                     //レイアウトをR.layout.sampleに変更する
                     getLayoutInflater().inflate(R.layout.view2, layout);
-                    count=2;
-                }else if(count == 2){
-                    LinearLayout layout = (LinearLayout)findViewById(R.id.linearlayout2);
+                    count = 2;
+                } else if (count == 2) {
+                    LinearLayout layout = (LinearLayout) findViewById(R.id.linearlayout2);
+                    layout.removeAllViews();
+                    getLayoutInflater().inflate(R.layout.view3, layout);
+                    count = 3;
+                } else if (count == 3) {
+                    LinearLayout layout = (LinearLayout) findViewById(R.id.linearlayout3);
                     layout.removeAllViews();
                     getLayoutInflater().inflate(R.layout.view1, layout);
-                    count=1;
+                    count = 1;
                 }
             }
+
+
         });
+
     }
 }
 
